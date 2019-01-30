d2ebbef51155d3801636ddfdf9801ceea0397120
wangguang <wangguang@pyramidvr.cn>
2019-01-31 00:29:42 +0800

    first commit

---
+++ AppIconVersion/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppIconVersion
+//
+//  Created by yellow on 2019/1/30.
+//  Copyright © 2019 yellow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AppIconVersion/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AppIconVersion
+//
+//  Created by yellow on 2019/1/30.
+//  Copyright © 2019 yellow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
